b40e437b5ff32d76d7a730a0491862f4adfd83da
Enzo Giachetti <165847571+EnzoGiachetti@users.noreply.github.com>
2024-08-17 10:59:07 -0300

    First Commit

---
+++ assets/js/global/global.js
@@ -0,0 +1,7 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    var actualPageElement = document.getElementById('actual-page');
+    if (actualPageElement) {
+        actualPageElement.textContent = 'Visão Geral';
+    }
+});
